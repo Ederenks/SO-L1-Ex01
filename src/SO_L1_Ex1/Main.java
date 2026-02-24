9725a227832e220c7d97e30d18e05a5641db0ed5
Edu <honorio.edu2020@gmail.com>
2026-02-24 17:41:47 -0300

    fim

--- src/SO_L1_Ex1/Main.java
+++ src/SO_L1_Ex1/Main.java
@@ -3,7 +3,7 @@ package SO_L1_Ex1;
 public class Main {
 	public static void main(String[]args) {
 		Controller cont = new Controller();
-		int [] vetor = new int[10000];
+		int [] vetor = new int[100000];
 		cont.aplicar(vetor );
 		
 	}
